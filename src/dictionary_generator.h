7005ccd1acb5c9d4d1f86e45973fd9cf908e228c
araiguma97 <tetra1738@gmail.com>
2020-05-01 15:48:58 +0900

    辞書生成クラスをリネーム

---
+++ src/dictionary_generator.h
@@ -0,0 +1,15 @@
+/**
+ * @file dictionary_generator.h
+ */
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+
+class DictionaryEditor {
+public:
+    void createDictionary(const std::string& find_file_path);
+    bool readDictionary(std::map<std::string, int>* dict);
+};
+
